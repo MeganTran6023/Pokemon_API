a6bf4beb849e12510e736cdc411b0755305d28f6
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2024-10-21 19:28:19 -0400

    in progress

---
+++ site.js
@@ -0,0 +1,31 @@
+//practice with first pokemon by id
+const pokemonCount = 152;
+var pokedex = {};
+
+
+window.onload = async function name() {
+    getPokemon(1);
+    
+}
+
+//fetch pokemon info
+async function getPokemon(num) {
+    let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
+
+    let res = await fetch(url);
+    let pokemon = await res.json();
+    console.log(pokemon);
+
+    let pokemonName = pokemon["name"];
+    let pokemonType = pokemon["types"];
+    let pokemonImg = pokemon["sprites"]["front_default"];
+
+    res = await fetch(pokemon["species"]["url"]);
+    let pokemonDesc = await res.json();
+
+    // console.log(pokemonDesc);
+    pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"];
+
+    pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc};
+
+}
